37bb82186e198df4c0e51af0b63c6f4042fbad6e
Miguel Angel Nivia O <85419069+Miguel-Angel-Nivia@users.noreply.github.com>
2023-09-07 22:55:19 -0500

    Create helloworld.cpp

---
+++ ExercisesC/helloworld.cpp
@@ -0,0 +1,10 @@
+// C++
+#include <iostream>
+
+// C
+// #include "stdio.h"
+
+int main(){
+  std::cout << "Hello World!!!" << std::endl;   // C++
+  // printf("Hello Wolrd!!!\n")                 // C
+}
